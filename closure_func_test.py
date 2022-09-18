3ac7b1de7ac4b9ab3a679735275c1363d703ac6c
마일혁 <ilhyeok@b4play.io>
2022-09-18 23:59:59 +0900

    2022-09-18-study

--- closure_func_test.py
+++ closure_func_test.py
@@ -1,26 +1,41 @@
-def in_cache(func):
-    cache = {}
-    def wrapper(n):
-        print(cache) ## !!!!
-        if n in cache:
-            return cache[n]
-        else:
-            cache[n] = func(n)
-            return cache[n]
-    return wrapper
+# def in_cache(func):
+#     cache = {}
+#     def wrapper(n):
+#         print(cache,n) ## !!!!
+#         if n in cache:
+#             return cache[n]
+#         else:
+#             cache[n] = func(n)
+#             return cache[n], print(cache[n],"here",func(n),n) ## !!!!
+#     return wrapper
 
 
-def factorial(n):
-    ret = 1
-    for i in range(1, n+1):
-        ret *= i
-    return ret
+# def factorial(n):
+#     ret = 1
+#     for i in range(1, n+1):
+#         ret *= i
+#     return ret
 
-factorial = in_cache(factorial)
-# cache의 변수를 저장
+# factorial = in_cache(factorial)
+# # cache의 변수를 저장
 
 
-print(factorial(3))
-print(factorial(5))
-print(factorial(10))
-print(factorial(11))
+# factorial(3)
+# factorial(5)
+# # print(factorial(10))
+# # print(factorial(11))
+
+
+
+def times_multiply(n):
+    def multiply(x):
+        print(x)
+        return n * x
+    return multiply
+
+
+times_3 = times_multiply(3)
+times_4 = times_multiply(4)
+
+print(times_3(5))
+print(times_4(5))
